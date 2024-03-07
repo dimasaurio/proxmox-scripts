fae5d161f2cd48502ca28f5dc617c0522753777b
dimasaurio <dimasaurio1@hotmail.com>
2024-03-07 19:48:50 +0100

    Create node_exporter_amd64.sh

---
+++ node_exporter_amd64.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Download and extract node_exporter
+cd /tmp
+wget https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
+tar xvfz node_exporter-1.7.0.linux-amd64.tar.gz
+sudo mv node_exporter-1.7.0.linux-amd64/node_exporter /usr/local/bin/
+
+# Create node_exporter user
+sudo useradd -rs /bin/false node_exporter
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+# Create systemd service file for node_exporter
+echo "[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/node_exporter.service
+
+# Reload systemd, enable and start node_exporter service
+sudo systemctl daemon-reload
+sudo systemctl enable --now node_exporter.service
+
+# Optionally, check the status
+sudo systemctl status node_exporter.service
