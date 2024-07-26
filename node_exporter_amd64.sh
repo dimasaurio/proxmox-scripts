ce24752326e8dcc85e1515243c89d41984ebca24
dimasaurio <dimasaurio1@hotmail.com>
2024-07-26 15:44:42 +0200

    Update node_exporter_amd64.sh

--- node_exporter_amd64.sh
+++ node_exporter_amd64.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Check if node_exporter exists and remove it if it does
+if [ -f /usr/local/bin/node_exporter ]; then
+    echo "Stopping and removing existing Node Exporter..."
+    sudo systemctl stop node_exporter.service
+    sudo systemctl disable node_exporter.service
+    sudo rm /usr/local/bin/node_exporter
+    sudo rm /etc/systemd/system/node_exporter.service
+    sudo systemctl daemon-reload
+fi
+
 # Download and extract node_exporter
 cd /tmp
 wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
 tar xvfz node_exporter-1.8.2.linux-amd64.tar.gz
-sudo mv node_exporter-1.8.2.linux-amd64.tar.gz/node_exporter /usr/local/bin/
+sudo mv node_exporter-1.8.2.linux-amd64/node_exporter /usr/local/bin/
 
 # Create node_exporter user
 sudo useradd -rs /bin/false node_exporter
